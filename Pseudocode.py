0145b8cd342bfce5bea3d8d7cea6112728e885b1
OhYeahJosh <130888170+OhYeahJosh@users.noreply.github.com>
2023-05-07 23:56:05 -0400

    Create Pseudocode.py

---
+++ Pseudocode.py
@@ -0,0 +1,45 @@
+ 
+Pseudocode  
+Import random  
+ 
+ 
+Def main  
+ 
+ 
+ 
+“Roll the dice” 
+ 
+ 
+Press enter to roll the dice 
+ 
+ 
+The values are…… 
+ 
+ 
+ Range for d1= 1-6 
+ 
+ 
+Range for d2= 1-6 
+ 
+ 
+Dice 1 = …. 
+ 
+ 
+Dice 2 = ….. 
+ 
+ 
+If sum equals 2,3 or 12 display lose  
+ 
+ 
+If sum equals 7 or 11 break loop  
+ 
+ 
+Display win or loss depending on the  
+ 
+ 
+Display total amount of wins or loss  
+ 
+ 
+Ask user to play again or quit.  
+ 
+Declare d1 and d2 
